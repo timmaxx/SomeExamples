d3200c3dc65c6d89be3704c7caab598b8b605171
“Maksim <m.n.timchenko@gmail.com>
2024-05-23 16:28:40 +0300

    make interface IState extends IStateContext

--- Head-First-Design-Patterns-master/src/main/java/headfirst/designpatterns/timmax/chapter10/timmax/p09/common/state/IState.java
+++ Head-First-Design-Patterns-master/src/main/java/headfirst/designpatterns/timmax/chapter10/timmax/p09/common/state/IState.java
@@ -1,6 +1,5 @@
 package headfirst.designpatterns.timmax.chapter10.timmax.p09.common.state;
 
-public interface IState {
-    void changeState(IState state);
+public interface IState extends IStateContext {
     void doOnEnter();
 }

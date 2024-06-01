b6b2ab852c1a5520e0f2e27094f820cc2dc723f3
“Maksim <m.n.timchenko@gmail.com>
2024-06-01 15:05:28 +0300

    create GumballDispenserTestDrive

---
+++ Head-First-Design-Patterns-master/src/test/java/headfirst/designpatterns/timmax/chapter10/timmax/p09/gumballdispenser/GumballDispenserTestDrive.java
@@ -0,0 +1,46 @@
+package headfirst.designpatterns.timmax.chapter10.timmax.p09.gumballdispenser;
+
+public class GumballDispenserTestDrive {
+    public static void main(String[] args) {
+        GumballDispenser gumballDispenser = new GumballDispenser();
+        System.out.println(gumballDispenser);
+        System.out.println("----");
+
+        gumballDispenser.releaseGumball();
+        System.out.println(gumballDispenser);
+        System.out.println("----");
+
+        gumballDispenser.refillGumballBox();
+        System.out.println(gumballDispenser);
+        System.out.println("----");
+
+        gumballDispenser.releaseGumball();
+        System.out.println(gumballDispenser);
+        System.out.println("----");
+
+        gumballDispenser.releaseGumball();
+        System.out.println(gumballDispenser);
+        System.out.println("----");
+
+        gumballDispenser.releaseGumball();
+        System.out.println(gumballDispenser);
+        System.out.println("----");
+
+        gumballDispenser.releaseGumball();
+        System.out.println(gumballDispenser);
+        System.out.println("----");
+
+        gumballDispenser.releaseGumball();
+        System.out.println(gumballDispenser);
+        System.out.println("----");
+
+        gumballDispenser.releaseGumball();
+        System.out.println(gumballDispenser);
+        System.out.println("----");
+
+        gumballDispenser.releaseGumball();
+        System.out.println(gumballDispenser);
+        System.out.println("----");
+
+    }
+}

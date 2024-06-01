eb6679013d7278186d1087c47ad8067cf7f54ea5
“Maksim <m.n.timchenko@gmail.com>
2024-06-01 14:22:19 +0300

    create CoinReceiverTestDrive (test drive for Coin receiver)

---
+++ Head-First-Design-Patterns-master/src/test/java/headfirst/designpatterns/timmax/chapter10/timmax/p09/coinreceiver/CoinReceiverTestDrive.java
@@ -0,0 +1,53 @@
+package headfirst.designpatterns.timmax.chapter10.timmax.p09.coinreceiver;
+
+public class CoinReceiverTestDrive {
+    public static void main(String[] args) {
+        CoinReceiver coinReceiver = new CoinReceiver();
+        System.out.println(coinReceiver);
+        System.out.println("----");
+
+        coinReceiver.ejectCoin();
+        System.out.println(coinReceiver);
+        System.out.println("----");
+
+        coinReceiver.insertCoin();
+        System.out.println(coinReceiver);
+        coinReceiver.insertCoin();
+        System.out.println(coinReceiver);
+        coinReceiver.ejectCoin();
+        System.out.println(coinReceiver);
+        System.out.println("----");
+
+        coinReceiver.insertCoin();
+        System.out.println(coinReceiver);
+        coinReceiver.acceptCoin();
+        System.out.println(coinReceiver);
+        System.out.println("----");
+
+        coinReceiver.insertCoin();
+        System.out.println(coinReceiver);
+        coinReceiver.acceptCoin();
+        System.out.println(coinReceiver);
+        System.out.println("----");
+
+        coinReceiver.insertCoin();
+        System.out.println(coinReceiver);
+        coinReceiver.acceptCoin();
+        System.out.println(coinReceiver);
+        System.out.println("----");
+
+        coinReceiver.insertCoin();
+        System.out.println(coinReceiver);
+        coinReceiver.acceptCoin();
+        System.out.println(coinReceiver);
+        System.out.println("----");
+
+        coinReceiver.insertCoin();
+        System.out.println(coinReceiver);
+        System.out.println("----");
+
+        coinReceiver.emptyCoinBox();
+        System.out.println(coinReceiver);
+        System.out.println("----");
+    }
+}

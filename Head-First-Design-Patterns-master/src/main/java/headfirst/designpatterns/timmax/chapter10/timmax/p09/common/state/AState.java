53b5a69c51dc4bc5c314d4273b6db98e08f5c504
“Maksim <m.n.timchenko@gmail.com>
2024-06-10 18:05:42 +0300

    refactor common state classes

--- Head-First-Design-Patterns-master/src/main/java/headfirst/designpatterns/timmax/chapter10/timmax/p09/common/state/AState.java
+++ Head-First-Design-Patterns-master/src/main/java/headfirst/designpatterns/timmax/chapter10/timmax/p09/common/state/AState.java
@@ -3,8 +3,6 @@ package headfirst.designpatterns.timmax.chapter10.timmax.p09.common.state;
 import java.util.HashSet;
 import java.util.Set;
 
-import headfirst.designpatterns.timmax.chapter10.timmax.p09.common.classes.Classes;
-
 public abstract class AState implements IState {
     private final StateAutomaton stateAutomaton;
 
@@ -15,18 +13,7 @@ public abstract class AState implements IState {
         this.setOfPairDestStateAndCanSwitchWithoutParams = new HashSet<>();
     }
 
-    private void checkPosibleToChangeState(IState state) {
-        for (PairDestStateAndCanSwitchWithoutParams pairDestStateAndCanSwitchWithoutParams : setOfPairDestStateAndCanSwitchWithoutParams) {
-            if (Classes.isInstanceOf(state, pairDestStateAndCanSwitchWithoutParams.destinationStateClass())
-                    && pairDestStateAndCanSwitchWithoutParams.canSwitchWithoutParams()) {
-                return;
-            }
-        }
-        throw new RuntimeException("You cannot change state from '" + this + "' to '" + state + "'!");
-    }
-
     protected final void changeState(AState state) {
-        checkPosibleToChangeState(state);
         stateAutomaton.changeState(state);
     }
 
--- Head-First-Design-Patterns-master/src/main/java/headfirst/designpatterns/timmax/chapter10/timmax/p09/common/state/StateAutomaton.java
+++ Head-First-Design-Patterns-master/src/main/java/headfirst/designpatterns/timmax/chapter10/timmax/p09/common/state/StateAutomaton.java
@@ -1,14 +1,27 @@
 package headfirst.designpatterns.timmax.chapter10.timmax.p09.common.state;
 
+import headfirst.designpatterns.timmax.chapter10.timmax.p09.common.classes.Classes;
+
 public abstract class StateAutomaton implements IStateAutomaton {
     private AState currentState;
 
     protected final void changeState(AState newState) {
-        if (this.currentState != null) {
-            this.currentState.doOnExit();
+        if (currentState != null) {
+            checkPosibleToChangeState(newState);
+            currentState.doOnExit();
         }
-        this.currentState = newState;
+        currentState = newState;
         newState.doOnEnter();
+    }
+
+    private void checkPosibleToChangeState(AState newState) {
+        for (PairDestStateAndCanSwitchWithoutParams pairDestStateAndCanSwitchWithoutParams : currentState.setOfPairDestStateAndCanSwitchWithoutParams) {
+            if (Classes.isInstanceOf(newState, pairDestStateAndCanSwitchWithoutParams.destinationStateClass())
+                    && pairDestStateAndCanSwitchWithoutParams.canSwitchWithoutParams()) {
+                return;
+            }
+        }
+        throw new RuntimeException("You cannot change state from '" + currentState + "' to '" + newState + "'!");
     }
 
     @Override

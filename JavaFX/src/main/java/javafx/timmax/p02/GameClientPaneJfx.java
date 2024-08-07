c89418bfa6aac633ef8039ea30751cd6ba574031
“Maksim <m.n.timchenko@gmail.com>
2024-08-07 19:17:33 +0300

    add view into class GameClientPaneJfx in JavaFX module

--- JavaFX/src/main/java/javafx/timmax/p02/GameClientPaneJfx.java
+++ JavaFX/src/main/java/javafx/timmax/p02/GameClientPaneJfx.java
@@ -1,17 +1,27 @@
 package javafx.timmax.p02;
 
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 public class GameClientPaneJfx extends VBox {
     public void createControls() {
-        Pane viewJfx = new Pane();
-        Button button = new Button("Some button");
-        button.setPrefWidth(200);
-        button.setMinWidth(200);
-        viewJfx.getChildren().add(button);
+        Pane viewJfx1 = new HBox();
+        Button button11 = new Button("button11");
+        button11.setPrefWidth(200);
+        button11.setMinWidth(200);
+        Button button12 = new Button("button12");
+        button12.setPrefWidth(300);
+        button12.setMinWidth(300);
+        viewJfx1.getChildren().addAll(button11, button12);
 
-        getChildren().add(viewJfx);
+        Pane viewJfx2 = new HBox();
+        Button button21 = new Button("button21");
+        button21.setPrefWidth(550);
+        button21.setMinWidth(550);
+        viewJfx2.getChildren().addAll(button21);
+
+        getChildren().addAll(viewJfx1, viewJfx2);
     }
 }
